c42a525c1925bbfec7b77a2a024e77c18e265817
admin <123>
2020-03-03 21:50:09 +0800

    第一次导入demo

---
+++ src/main/java/com/cssl/Test/test.java
@@ -0,0 +1,16 @@
+package com.cssl.Test;
+
+/**
+ * @ClassName test
+ * @Description TODO
+ * @Author 李祖波
+ * @Date 2020/3/2 23:06
+ * @Version 1.0
+ **/
+public class test {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Word!!!");
+    }
+
+}
