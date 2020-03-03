0969b7b5cb035aa3303de52dff82cddfb7b95f73
admin <123>
2020-03-03 22:12:13 +0800

    添加了实体类pojo

---
+++ src/main/java/com/cssl/pojo/users.java
@@ -0,0 +1,13 @@
+package com.cssl.pojo;
+
+/**
+ * @ClassName users
+ * @Description TODO
+ * @Author 李祖波
+ * @Date 2020/3/3 22:10
+ * @Version 1.0
+ **/
+public class users {
+    private String name;
+    private String pwd;
+}
